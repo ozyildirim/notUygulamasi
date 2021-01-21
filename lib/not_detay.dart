25270beea36e0c64eb6af6e519fddf1e26fb22d8
ozyildirim <kutay9622@gmail.com>
2021-01-22 01:38:47 +0300

    first

--- lib/not_detay.dart
+++ lib/not_detay.dart
@@ -14,7 +14,9 @@ class _NotDetayState extends State<NotDetay> {
   var formKey = GlobalKey<FormState>();
   List<Kategori> tumKategoriler;
   DatabaseHelper databaseHelper;
-  int kategoriID;
+  int kategoriID = 1;
+  int secilenOncelik = 0;
+  static var _oncelik = ["Düşük", "Orta", "Yüksek"];
 
   @override
   void initState() {
@@ -33,41 +35,136 @@ class _NotDetayState extends State<NotDetay> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.baslik),
-      ),
-      body: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 4, horizontal: 12),
-                margin: EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.redAccent, width: 2),
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-                child: tumKategoriler.length <= 0
-                    ? CircularProgressIndicator()
-                    : DropdownButton<int>(
-                        items: kategoriItemleriOlustur(),
-                        value: kategoriID,
-                        onChanged: (secilenKategoriID) {
-                          setState(() {
-                            kategoriID = secilenKategoriID;
-                          });
-                        }),
-              ),
-            ),
-          ],
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
+          title: Text(widget.baslik),
         ),
-      ),
-    );
+        body: tumKategoriler.length <= 0
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              "Kategori:",
+                              style: TextStyle(fontSize: 24),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 4, horizontal: 12),
+                            margin: EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Colors.redAccent, width: 2),
+                                borderRadius:
+                                    BorderRadius.all((Radius.circular(10)))),
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton(
+                                items: kategoriItemleriOlustur(),
+                                value: kategoriID,
+                                onChanged: (secilenKategoriID) {
+                                  setState(() {
+                                    kategoriID = secilenKategoriID;
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              hintText: "Not Başlığını Giriniz:",
+                              labelText: "Başlık",
+                              border: OutlineInputBorder()),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          maxLines: 4,
+                          decoration: InputDecoration(
+                              hintText: "Not İçeriğini Giriniz:",
+                              labelText: "İçerik",
+                              border: OutlineInputBorder()),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "Öncelik: ",
+                              style: TextStyle(fontSize: 24),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 2, horizontal: 12),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    color: Colors.redAccent, width: 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10))),
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton<int>(
+                                items: _oncelik.map((oncelik) {
+                                  return DropdownMenuItem<int>(
+                                    child: Text(
+                                      oncelik,
+                                      style: TextStyle(fontSize: 24),
+                                    ),
+                                    value: _oncelik.indexOf(oncelik),
+                                  );
+                                }).toList(),
+                                value: secilenOncelik,
+                                onChanged: (secilenOncelikID) {
+                                  setState(() {
+                                    secilenOncelik = secilenOncelikID;
+                                  });
+                                },
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      ButtonBar(
+                        alignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          RaisedButton(
+                            onPressed: () {},
+                            child: Text("Vazgeç"),
+                            color: Colors.redAccent.shade200,
+                          ),
+                          RaisedButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Kaydet",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            color: Colors.redAccent.shade700,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ));
   }
 
   List<DropdownMenuItem<int>> kategoriItemleriOlustur() {
-    List<DropdownMenuItem<int>> kategoriler = [];
-
     return tumKategoriler
         .map((kategori) => DropdownMenuItem(
               child: Padding(
